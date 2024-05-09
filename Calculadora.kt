efd6bc3209d8b2a42f3a87bcbd4e172f0948eebc
Daniel.llopezz <166647395+Danielwv21@users.noreply.github.com>
2024-05-09 17:45:29 -0300

    Create Calculadora.kt

---
+++ Calculadora.kt
@@ -0,0 +1,17 @@
+class Calculadora {
+    fun somar(a: Int, b: Int): Int {
+        return a + b
+    }
+
+    fun subtracao(a: Int, b: Int): Int {
+        return a - b
+    }
+
+    fun multiplicar(a: Int, b: Int): Int {
+        return a * b
+    }
+
+    fun dividir(a: Int, b: Int): Int {
+       return a / b
+}
+}
