b76b8db40fc40b57428b21b30e839c462078ae1c
Daniel.llopezz <166647395+Danielwv21@users.noreply.github.com>
2024-05-13 21:03:07 -0300

    Create Cachorro.kt

---
+++ Cachorro.kt
@@ -0,0 +1,5 @@
+class Cachorro (val nome: String, val raca: String, var idade: Int){
+    fun apresentar(){
+        println("O nome, raça e idade são, respectivamente $nome, $raca e $idade")
+    }
+}
